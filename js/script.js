f661d6754cee014be01d7b37cfbb2b81d4823f48
LiamBartolini <liambartolini@gmail.com>
2021-02-09 22:08:50 +0100

    css cleaning code

--- js/script.js
+++ js/script.js
@@ -1,120 +1,44 @@
-var exist = false;
 var contatore = 0;
-var persone = new Array(10);
+var persone = new Array(20); // Imposto un massimo di 20 nomi
 var blocco;
 var indice = 0;
 var nomeIntero;
              
 jQuery(function () {
 
+    // listener per aggiungere un nome
     jQuery("#btnAccetta, #btnChiudi").on("click", function () {
         if (this.id == "btnAccetta") {
             addingInput(nomeIntero);
         }
     });
 
+    // prendo i dati
     jQuery("#btnInserisci").on("click", function () {
         var nome = jQuery("#txtNome").val().trim();
         var cognome = jQuery("#txtCognome").val().trim();
         nomeIntero = nome + " " + cognome;
         
+        // controllo che i dati inseriti siano unici
         if (checkInput(nomeIntero)) {
             if (contatore > 0) {
                 // Se non esiste allora chiedo la conferma e lo aggiungo
                 activeModalAccettazione("Per confermare l'inserimento di " + nomeIntero + " cliccare su 'accetto'!");
-                // jQuery("#btnAccetta, #btnChiudi").on("click", function () {
-                //     if (this.id == "btnAccetta") {
-                //         addingInput(nomeIntero);
-                //     }
-                // });
-
-                contatore++;
-            } else if (contatore == 0) { 
+            } else { 
                 activeModalAccettazione("Per confermare l'inserimento di " + nomeIntero +" cliccare su 'accetto'!");
-                // jQuery("#btnAccetta, #btnChiudi").on("click", function () {
-                //     if (this.id = "btnAccetta") {
-                //         addingInput(nomeIntero);
-                //     }
-                // });
-                contatore++;
             }
-            // if (contatore == 0) {
-                // per il primo nome inserito lo chiede la conferma ma non verifica se è gia stato inserito
-                // activeModal("Per confermare l'inserimento cliccare su 'accetto'!", "Conferma Input");
-                // jQuery("#btnAccetta, #btnChiudi").on("click", function () {
-                //     if (this.id == "btnAccetta") {
-                //         addingInput(nomeIntero);
-                //     }
-                // });
-                // jQuery("#btnAccetta").off("click");
-
-                // if (jQuery("#btnAccetta").on("click"))
-                //     addingInput(nomeIntero);
-                // // jQuery("#btnAccetta").on("click", addingInput(nomeIntero));   
-            // } else {
-                // verifico se l'input sia corretto e non gia inserito
-                // for (var i = 0; i < persone.length; i++) {
-                //     if (nomeIntero == persone[i]) {
-                //         activeModal("Il nome è gia stato inserito!", "Errore input");
-                //         uguale = true;
-                //     }
-                // }
-
-                // if (!uguale) {
-                    // Se non è stato inserito attivo il modale
-                    // activeModal("Per confermare l'inserimento cliccare su 'accetto'!", "Conferma Input");
-                    // if (jQuery("#btnAccetto").on("click"))
-                    //     addingInput(nomeIntero);
-
-                    // jQuery("#btnAccetta, #btnChiudi").on("click", function () {
-                    //     if (this.id == "btnAccetta") {
-                    //         addingInput(nomeIntero);
-                    //         // // Lo aggiungo alla lista di persone
-                            // persone[indice] = nomeIntero;
-                            // // Creo un blocco gli aggiungo una clase e lo appendo al div numero 7
-                            // blocco = jQuery("<p>" + nomeIntero + "</p>");
-                            // blocco.addClass("inseriti");
-                            // blocco.appendTo("#div7");
-                    //     }
-                    // });
-
-                    // jQuery("#btnAccetta, #btnChiudi").on("click", function (){
-                    //     if (this.id == "btnAccetta") {}
-                    //     else if (this.id == "btnChiudi") {}
-                    // });
-
-                    // jQuery("#btnAccetta").on("click", addingInput(nomeIntero));
-
-                    // Controllo se è stato premuto il stato accetta
-                    // jQuery("#btnAccetta").on("click", function () )
-                    //     if (!uguale) {
-                    //         // Lo aggiungo alla lista di persone
-                    //         persone.push(nomeIntero);
-                    //         // Creo un blocco gli aggiungo una clase e lo appendo al div numero 7
-                    //         blocco = jQuery("<p>" + nomeIntero + "</p>");
-                    //         blocco.addClass("inseriti");
-                    //         blocco.appendTo("#div7");
-                    //     }
-                    // }
-            //     }
-            //     uguale = false;
-            //     indice++;
-            // }
-            // contatore++;
+            contatore++;
         }
     });
 });
 
-// Metodo per attivare il modal di bootstrap
+// Metodo per attivare il modal
 function activeModalAccettazione(testo) {
-    // jQuery("#btnInserisci").on("click", function () {
-    //     jQuery("#testo-modal").html(testo);
-    //     jQuery("#modal-accettazione").modal("toggle");
-    // });
     jQuery("#testo-modal").html(testo);
     jQuery("#modal-accettazione").modal("toggle");
 }
 
+// richiamo il modal per l'errore
 function activeModalError(testo) {
     jQuery("#modal-error-text").html(testo);
     jQuery("#error-modal").modal("toggle");
